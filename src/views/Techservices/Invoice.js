e259cdb062f52ce4d21d2d588d0d610367914d55
Charles Cifuentes <charlescifuentes@colombiaweb.co>
2019-09-26 23:39:14 -0500

    Trabajando en Invoice Component

--- src/views/Techservices/Invoice.js
+++ src/views/Techservices/Invoice.js
@@ -9,19 +9,46 @@ class Invoice extends Component {
         
         return (
             <div className="animated fadeIn">
-                <Card>
-                    <CardHeader>
-                        <i className="fa fa-align-justify"></i> IMPRIMIR ORDEN DE SERVICIO
-                    </CardHeader>
-                    <CardBody>
-                        <h1>{ts_id}</h1>
-                        <h2>{customer_firstname}</h2>
-                    </CardBody>
-                    <CardFooter>
-                        
-                        
-                    </CardFooter>
-                </Card>
+                <Row>
+                    <Col xs="12" sm="12" md="12">
+                        <Card>
+                            <CardHeader>
+                                <i className="fa fa-align-justify"></i> IMPRIMIR ORDEN DE SERVICIO
+                            </CardHeader>
+                            <CardBody>
+                                <Row>
+                                    <Col xs="12" sm="6" md="6">
+                                        <div>Logo Empresa</div>
+                                    </Col>
+                                    <Col xs="12" sm="6" md="6" className="d-flex justify-content-end">
+                                        <h2>TALLER TIME</h2>
+                                        <div>Carrera 25 # 31-32</div>
+                                        <div>321 345 4565</div>
+                                        <div>Tuluá, Valle</div>
+                                    </Col>
+                                </Row>
+                                <hr />
+                                <Row>
+                                    <Col xs="12" sm="6" md="6">
+                                        <h1>CLIENTE</h1>
+                                        <div>{customer_firstname}</div>
+                                        <div>321 345 4565</div>
+                                        <div>Tuluá, Valle</div>
+                                    </Col>
+                                    <Col xs="12" sm="6" md="6">
+                                        <h2>#: {ts_id}</h2>
+                                        <div>Fecha: </div>
+                                        <div>321 345 4565</div>
+                                        <div>Tuluá, Valle</div>
+                                    </Col>
+                                </Row>
+                            </CardBody>
+                            <CardFooter>
+                                <h2>Footer</h2>
+                            </CardFooter>
+                        </Card>
+                    </Col>
+                </Row>
             </div>
         )
     }
